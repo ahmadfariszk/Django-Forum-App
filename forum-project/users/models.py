91db062640bddff69290be6a60ea9ec9e174666f
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-12-02 22:14:11 +0800

    Added user model

--- forum-project/users/models.py
+++ forum-project/users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser #default python user class
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    
+    # Use the email for authentication instead of the username
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'name']
+    
+    def __str__(self):
+        return self.email
