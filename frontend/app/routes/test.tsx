af6accf1b583ce3150b042f034469a083b087e9a
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2024-12-04 02:43:51 +0800

    Added shadcn

---
+++ frontend/app/routes/test.tsx
@@ -0,0 +1,11 @@
+import {TestPage} from "~/test/TestPage"
+
+const Home = () => {
+    return (
+      <div>
+        <TestPage/>
+      </div>
+    )
+  }
+  
+export default Home
---
+++ frontend/app/test/TestPage.tsx
@@ -0,0 +1,33 @@
+import { Button } from "~/shared/components/Button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/shared/components/Card"
+import { Input } from "~/shared/components/Input"
+import { Label } from "~/shared/components/Label"
+
+export const TestPage = () => {
+    return(
+        <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+    )
+}
